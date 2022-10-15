b05bd088ee1d6f4556557e221ec8d5ec5d128fe7
abdelrahman esam <abdelrahmane792@outlook.com>
2022-10-15 16:29:20 +0200

    connection string

--- attendance_system/Startup.cs
+++ attendance_system/Startup.cs
@@ -1,6 +1,8 @@
+using attendance_system.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace attendance_system
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DbContext configuration
+            services.AddDbContext<attendance_systemContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+
             services.AddControllersWithViews();
         }
 
